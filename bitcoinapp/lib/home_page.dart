fb365f6ed46a1ea4a99661746c6567ee95887ab9
Pranav Chopra <41725998+chopraji94@users.noreply.github.com>
2022-03-25 23:33:04 +0530

    Completed BitCoin App

--- bitcoinapp/lib/home_page.dart
+++ bitcoinapp/lib/home_page.dart
@@ -1,49 +1,107 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
+  final List currencies;
+
+  MyHomePage(this.currencies);
+
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  Widget _textWidgetComposer() {
-    return new ListView.builder(
-      itemCount: 1,
-      itemBuilder: (context,int index){
-        return new Container(
-          margin: const EdgeInsets.symmetric(vertical: 15.0),
-          child: new Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
+  final List<MaterialColor> _colors = [Colors.blue, Colors.indigo, Colors.red];
+
+  Widget avatarWidget(int index, MaterialColor color) {
+    return new CircleAvatar(
+      backgroundColor: color,
+      child: Text(widget.currencies[index]['name'][0],style: new TextStyle(color: Theme.of(context).accentColor),),
+    );
+  }
+
+  Widget coinNameWidget(int index) {
+    return new Text(
+      widget.currencies[index]['name'],
+      style: Theme.of(context).textTheme.subtitle1,
+    );
+  }
+
+  Widget coinPriceWidget(String price, String percentageChange) {
+    return new Container(
+      margin: const EdgeInsets.only(top: 5.0),
+      child: new Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Row(
             children: <Widget>[
-              new Container(
-                margin: const EdgeInsets.only(right: 10.0),
-                child: new CircleAvatar(child: Text("P")),
-              ),
-              new Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  new Text(
-                    "Pranav Chopra",
-                    style: Theme.of(context).textTheme.subtitle1,
-                  ),
-                  new Container(
-                    margin: const EdgeInsets.only(top: 5.0),
-                    child: Text("Hello, How are you"),
-                  ),
-                ],
-              ),
+              new Text(price),
+              coinArrowWidget(percentageChange)
             ],
           ),
-        );
-      }
+          coinTextPRiceWIdget(percentageChange)
+        ],
+      ),
     );
+  }
+
+  Widget coinArrowWidget(String percentageChange) {
+    if (double.parse(percentageChange) > 0) {
+      return new Icon(Icons.arrow_circle_up, color: Colors.green);
+    } else {
+      return new Icon(Icons.arrow_circle_down, color: Colors.red);
+    }
+  }
+
+  Widget coinTextPRiceWIdget(String percentageChange) {
+    if (double.parse(percentageChange) > 0) {
+      return new Text(percentageChange,
+          style: new TextStyle(color: Colors.green));
+    } else {
+      return new Text(percentageChange,
+          style: new TextStyle(color: Colors.red));
+    }
+  }
+
+  Widget _textWidgetComposer() {
+    return new ListView.builder(
+        itemCount: widget.currencies.length,
+        itemBuilder: (context, int index) {
+          return new Container(
+            margin: const EdgeInsets.symmetric(vertical: 15.0),
+            child: new Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                new Container(
+                    margin: const EdgeInsets.only(left: 8.0, right: 10.0),
+                    child:
+                        avatarWidget(index, _colors[index % _colors.length])),
+                new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    coinNameWidget(index),
+                    coinPriceWidget(
+                        widget.currencies[index]['quote']['USD']['price']
+                            .toString(),
+                        widget.currencies[index]['quote']['USD']
+                                ['percent_change_1h']
+                            .toString())
+                  ],
+                ),
+              ],
+            ),
+          );
+        });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: new AppBar(
-        title: new Text("Digital currency detail's",style: new TextStyle(color: Colors.white),),
+        title: new Text(
+          "Digital currency detail's",
+          style: new TextStyle(color: Colors.white),
+        ),
       ),
       body: _textWidgetComposer(),
     );
--- bitcoinapp/lib/main.dart
+++ bitcoinapp/lib/main.dart
@@ -1,16 +1,36 @@
 import 'package:bitcoinapp/home_page.dart';
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
-void main() {
-  runApp(new MyApp());
+void main() async {
+  List currencies = await getCurrencies();
+  runApp(new MyApp(currencies));
 }
 
 class MyApp extends StatelessWidget {
+  final List _currencies;
+
+  MyApp(this._currencies);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: new MyHomePage(),
+        home: new MyHomePage(_currencies),
         theme: new ThemeData(
-            primarySwatch: Colors.lightBlue, brightness: Brightness.light));
+            primarySwatch: Colors.lightBlue, brightness: Brightness.dark));
   }
+}
+
+Future<List> getCurrencies() async {
+  String cryptoUrl =
+      "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest";
+
+  Response response = await http.get(Uri.parse(cryptoUrl),
+      headers: {'X-CMC_PRO_API_KEY': '387e7cc1-01a8-4463-bb5e-26d9cf6b29ed'});
+  var jsonResponse = json.decode(response.body);
+  var onlyData = jsonResponse['data'] as List;
+  return onlyData;
 }
